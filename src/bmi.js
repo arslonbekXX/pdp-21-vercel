c61c56265c39f63065edb57b84f58c291c00d052
arslonbekXX <code.2307sabia@gmail.com>
2025-04-15 15:59:50 +0500

    for challange

--- src/bmi.js
+++ src/bmi.js
@@ -15,9 +15,28 @@ function calculateBMI(weight, height) {
  * - Underweight: BMI < 18.5
  * - Normal: 18.5 <= BMI < 25
  * - Overweight: 25 <= BMI < 30
- * - Obese: BMI >= 30
+ * - Obese: 30 <= BMI < 35
+ * - Extremely Obese: BMI >= 35
  *
  * @param {number} bmi - The BMI value
  * @returns {string} The BMI status (Underweight, Normal, Overweight, or Obese)
  */
-function getBMIStatus(bmi) {}
+function getBMIStatus(bmi) {
+	return 'Underweight';
+}
+
+const name1 = 'Kent';
+const weight1 = 70;
+const height1 = 1.75;
+
+const name2 = 'John';
+const weight2 = 90;
+const height2 = 1.8;
+
+const name3 = 'Mark';
+const weight3 = 85;
+const height3 = 1.7;
+
+console.log('Kent:  BMI is 18 and status is ', getBMIStatus(18));
+console.log('John:  BMI is 18 and status is ', getBMIStatus(18));
+console.log('Mark:  BMI is 18 and status is ', getBMIStatus(18));
