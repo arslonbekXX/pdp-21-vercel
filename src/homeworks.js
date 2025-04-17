3f206c011a9f2b796efd45f14e6ff3b5c2a2d054
arslonbekXX <code.2307sabia@gmail.com>
2025-04-17 18:16:45 +0500

    lesson-40 finish-codes

--- src/homeworks.js
+++ src/homeworks.js
@@ -2,20 +2,44 @@
  *
  * @param {string} str
  * @param {string} search
+ * @param {number} position
  * @returns {number}
  */
-function indexOfString(str, search) {
-	// your code
+function indexOfString(str, search, position = 0) {
+	for (let i = position; i < str.length; i++) {
+		let sub = '';
+		for (let j = i; j < i + search.length; j++) {
+			sub += str[j];
+		}
+		if (sub === search) {
+			return i;
+		}
+	}
+
+	return -1;
 }
 
 /**
  *
  * @param {string} str
  * @param {string} search
+ * @param {number} position
  * @returns {number}
  */
-function lastIndexOfString(str, search) {
-	// your code
+function lastIndexOfString(str, search, position = 0) {
+	let idx = -1;
+
+	for (let i = position; i < str.length; i++) {
+		let sub = '';
+		for (let j = i; j < i + search.length; j++) {
+			sub += str[j];
+		}
+		if (sub === search) {
+			idx = i;
+		}
+	}
+
+	return idx;
 }
 
 /**
@@ -59,9 +83,12 @@ function subStr(str, startIdx, count) {
 	// your code
 }
 
-console.log(indexOfString('PDP BEST UNIVERSITY', 'BEST')); // 4
+// console.log(indexOfString('PDP BEST UNIVERSITY BEST OF THE BEST', 'BEST', 100)); // 4
 console.log(lastIndexOfString('PDP BEST UNIVERSITY', 'E')); // 13
-console.log(sliceString('PDP BEST UNIVERSITY', 4, 13)); // BEST UNIVERSITY
-console.log(startsWithString('PDP BEST UNIVERSITY', 'PDP')); // true
-console.log(endsWithString('PDP BEST UNIVERSITY', 'UNIVERSITY')); // true
-console.log(subStr('PDP BEST UNIVERSITY', 4, 4)); // BEST
+// console.log(sliceString('PDP BEST UNIVERSITY', 4, 13)); // BEST UNIVERSITY
+// console.log(startsWithString('PDP BEST UNIVERSITY', 'PDP')); // true
+// console.log(endsWithString('PDP BEST UNIVERSITY', 'UNIVERSITY')); // true
+// console.log(subStr('PDP BEST UNIVERSITY', 4, 4)); // BEST
+
+// let a = 'arslonbek bek bek';
+// const idx = a.indexOf('bek', 7); // 10
