3294dcd5c91644d0cd0047eede2b981c92675f05
arslonbekXX <code.2307sabia@gmail.com>
2025-06-05 11:44:07 +0500

    lesson-60 finish-codes

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-// import "./class"
-// import './set';
-// import './traffic-light';
-// import './challenges/challenge-2';
-// import './event-phases';
-// import './custom-events';
-// import './form-events';
-import './keyboard-events';
