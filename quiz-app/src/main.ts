0051ac570597677fbc2e0035a1f590cc7d817c63
arslonbekXX <code.2307sabia@gmail.com>
2025-06-12 09:06:09 +0500

    quiz-app v1 finished

--- quiz-app/src/main.ts
+++ quiz-app/src/main.ts
@@ -1,13 +1,14 @@
 import { MAX_QUESTIONS_COUNT } from './constants';
 import type { Question } from './types';
 import { generateQuestion, getStatus } from './utils';
-// HANDLE FUNCTIONS
+import { num1Elm, operationElm, num2Elm, variantlarElm, variantBtns } from './elements';
 
+// VARIABLES
 const questions: Question[] = [];
 let question: Question;
 
+// HANDLE FUNCTIONS
 function handleSelectAnswer(event: MouseEvent) {
-	const variantlarElm = document.querySelector('.variantlar') as HTMLDivElement;
 	variantlarElm.style.pointerEvents = 'none';
 
 	const variantBtn = event.target as HTMLButtonElement;
@@ -21,20 +22,14 @@ function handleSelectAnswer(event: MouseEvent) {
 
 // UI FUNCTIONS
 function renderQuestion() {
-	const num1Elm = document.querySelector('#num1') as HTMLDivElement;
-	const operationElm = document.querySelector('#operation') as HTMLDivElement;
-	const num2Elm = document.querySelector('#num2') as HTMLDivElement;
-
 	num1Elm.innerText = question.number1.toString();
 	operationElm.innerText = question.operation;
 	num2Elm.innerText = question.number2.toString();
 }
 
 function renderAnswers() {
-	const variantlarElm = document.querySelector('.variantlar') as HTMLDivElement;
 	variantlarElm.style.pointerEvents = 'unset';
 
-	const variantBtns = document.querySelectorAll('.variant-btn') as NodeListOf<HTMLButtonElement>;
 	for (let i = 0; i < variantBtns.length; i++) {
 		const variantBtn = variantBtns[i];
 		const answer = question.answers[i];
@@ -66,8 +61,6 @@ function init() {
 
 	renderQuestion();
 	renderAnswers();
-
-	console.log('questions = ', questions);
 }
 
 window.addEventListener('load', init);
