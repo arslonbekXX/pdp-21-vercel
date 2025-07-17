10ef1f6e67e1bb6471b52c3bf80c2db9a8753baf
arslonbekXX <code.2307sabia@gmail.com>
2025-07-17 12:05:13 +0500

    inital codes

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-const heading = document.querySelector('h1') as HTMLHeadingElement;
-const btn = document.querySelector('button')!;
-
-let count = 0;
-btn.onclick = () => {
-	heading.innerText = `Count: ${++count}`;
-};
