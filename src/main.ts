d361288a350cad90d7624c1f090600f23ff318e7
arslonbekXX <code.2307sabia@gmail.com>
2025-08-09 12:44:33 +0500

    lesson-87 finish-codes

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+// function createIncrement(initialValue: number) {
+// 	let count = initialValue;
+// 	return function () {
+// 		count++;
+// 		return count;
+// 	};
+// }
+
+// const increment = createIncrement(10);
+
+// console.dir(increment());
+// console.dir(increment());
+// console.dir(increment());
+// console.dir(increment());
+
+function createTrafficLight() {
+	let status = "OFF";
+
+	return {
+		getStatus() {
+			return status;
+		},
+		setStatus(newStatus: string) {
+			status = newStatus;;
+		},
+		on() {
+			status = "ON";
+		},
+		off() {
+			status = "OFF";
+		},
+	};
+}
+
+const trafficLight = createTrafficLight();
+
+trafficLight.on();
+console.log(trafficLight.getStatus());
+trafficLight.off();
+console.log(trafficLight.getStatus());
+
+function createFunctions(count: number) {
+	const functions = [];
+
+	for (let i = 1; i <= count; i++) {
+		functions.push(() => i);
+	}
+
+	return functions;
+}
+
+const fns = createFunctions(5);
+
+console.log(fns[0]()); // 1
+console.log(fns[1]()); // 2
+console.log(fns[2]()); // 3
+console.log(fns[3]()); // 4
+console.log(fns[4]()); // 5
