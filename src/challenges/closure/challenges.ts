d9d379ae6d1ca939b90e3d7910b78e46b0f185a8
arslonbekXX <code.2307sabia@gmail.com>
2025-08-12 12:58:08 +0500

    lesson-88 finish-codes

--- src/challenges/closure/challenges.ts
+++ src/challenges/closure/challenges.ts
@@ -163,11 +163,11 @@
 
 console.log("======== CHALLENGE-10 ========");
 function partial(fn: (...args: number[]) => number, ...args: number[]) {
-	return function (n: number) {
-		return fn(n, ...args);
+	return function (...nums: number[]) {
+		return fn(...nums, ...args);
 	};
 }
 
-const add = (a: number, b: number, c: number) => a + b + c;
-const summa = partial(add, 2, 3, 4, 5, 6, 7);
-console.log(summa(10, 15, 20, 30)); // 15
+const add = (...args: number[]) => args.reduce((a, c) => a + c);
+const summa = partial(add, 1);
+console.log(summa(10)); // 11
