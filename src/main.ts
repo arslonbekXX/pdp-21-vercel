c250911b2848388ad91d2a5d3d12f78de1e98ee2
arslonbekXX <code.2307sabia@gmail.com>
2025-08-16 13:13:52 +0500

    lesson-90 finish-codes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 function createIncrement(initialValue: number, interval: number) {
+	const results: number[] = [];
+	let value = initialValue;
 	return {
-		increment(step: number) {},
-		start() {},
+		increment(step: number) {
+			value += step;
+			results.push(value);
+		},
+		start() {
+			for (let i = 0; i < results.length; i++) {
+				const result = results[i];
+				const delay = interval * (i + 1);
+				setTimeout(() => console.log(result), delay);
+			}
+		},
 	};
 }
 
