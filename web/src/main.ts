9be0a28543b203ca0f1dfb558dfa28884db0136d
arslonbekXX <code.2307sabia@gmail.com>
2025-09-20 21:16:47 +0500

    lesson-103 finish-codes

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,7 @@ import { faker } from "@faker-js/faker";
 
 const btnPostRegister = document.querySelector(".btn-post-register") as HTMLButtonElement;
 const btnPostCreateGame = document.querySelector(".btn-post-create-game") as HTMLButtonElement;
+const btnPostLogin = document.querySelector(".btn-post-login") as HTMLButtonElement;
 const baseURL = "http://localhost:4000";
 
 let user: User | null = null;
@@ -32,12 +33,51 @@ const handleRegister = async (e: MouseEvent) => {
 	}
 };
 
+const handleLogin = async (e: MouseEvent) => {
+	const btn = e.target as HTMLButtonElement;
+	btn.innerHTML += "...";
+	const loginBody: Pick<User, "email" | "password"> = {
+		email: "",
+		password: "",
+	};
+
+	await sleep();
+
+	try {
+		const res = await fetch(`${baseURL}/auth/login`, {
+			method: "POST",
+			body: JSON.stringify(loginBody),
+			headers: { "Content-Type": "application/json" },
+		});
+		const data = await res.json();
+		user = data.player;
+	} catch (error) {
+		console.error("error = ", error);
+	} finally {
+		btn.innerHTML = btn.innerHTML.replace("...", "");
+	}
+};
+
 const handleCreateGame = async (e: MouseEvent) => {
 	const btn = e.target as HTMLButtonElement;
 	btn.innerHTML += "...";
 
-	console.log("user = ", user);
+	if (!user) return alert("User is not logged in");
+	try {
+		const res = await fetch(`${baseURL}/games`, {
+			method: "POST",
+			body: JSON.stringify({ player1Id: user.id }),
+			headers: { "Content-Type": "application/json" },
+		});
+		const data = await res.json();
+		console.log("data = ", data);
+	} catch (error) {
+		console.error("error = ", error);
+	} finally {
+		btn.innerHTML = btn.innerHTML.replace("...", "");
+	}
 };
 
 btnPostRegister.addEventListener("click", handleRegister);
 btnPostCreateGame.addEventListener("click", handleCreateGame);
+btnPostLogin.addEventListener("click", handleLogin);
